3536cca0c5adbd5ee0d3e1ddd4be106b36909b91
GromovIvan <grom_grom2005@mail.ru>
2023-12-20 01:08:00 +0300

    Добавьте файлы проекта.

---
+++ Notebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab4
+{
+    public class NoteBook
+    {
+        private List<Contact> _contacts = new List<Contact>();
+
+        public void AddNewContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return _contacts;
+        }
+    }
+}
